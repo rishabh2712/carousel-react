94b5b9b4122184dfe343df80cb8c5c65d525c20d
rishabh <rishabhbansal964@gmail.com>
2019-09-02 21:13:20 +0530

    Updates

---
+++ precache-manifest.4f855cf6fd73dca85ccbb3bfb313537b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0faa4597fa8b8f484546",
+    "url": "/shaadi-assignment/static/js/runtime~main.0faa4597.js"
+  },
+  {
+    "revision": "5e47c0aa0929ab2fee58",
+    "url": "/shaadi-assignment/static/js/main.5e47c0aa.chunk.js"
+  },
+  {
+    "revision": "5e6324515084c2f2798e",
+    "url": "/shaadi-assignment/static/js/1.5e632451.chunk.js"
+  },
+  {
+    "revision": "5e47c0aa0929ab2fee58",
+    "url": "/shaadi-assignment/static/css/main.29d7b666.chunk.css"
+  },
+  {
+    "revision": "5e6324515084c2f2798e",
+    "url": "/shaadi-assignment/static/css/1.e7f62c72.chunk.css"
+  },
+  {
+    "revision": "e72f3308a23ebcecfa2a8831ff3693f3",
+    "url": "/shaadi-assignment/index.html"
+  }
+];
